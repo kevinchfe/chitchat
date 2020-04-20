fb1067ba61b77653b47b397f2bee3c5afb9e16db
kevin <546598819@qq.com>
2020-04-20 11:23:09 +0800

    全局配置

--- databases/mysql.go
+++ databases/mysql.go
@@ -1,7 +1,9 @@
 package databases
 
 import (
+	config2 "chitchat/config"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
@@ -10,7 +12,11 @@ var Db *sql.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/chitchat?parseTime=true")
+	config := config2.LoadConfig()
+	driver := config.Db.Driver
+	source := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true", config.Db.User, config.Db.Password, config.Db.Address, config.Db.Database)
+	//Db, err = sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/chitchat?parseTime=true")
+	Db, err = sql.Open(driver, source)
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	config2 "chitchat/config"
 	. "chitchat/routes"
 	"log"
 	"net/http"
@@ -11,15 +12,18 @@ func main() {
 }
 
 func startWebServer(port string) {
+	// 初始化全局配置
+	config := config2.LoadConfig()
+
 	r := NewRouter()
 	// 静态资源
-	assets := http.FileServer(http.Dir("public"))
+	assets := http.FileServer(http.Dir(config.App.Static))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", assets))
 
 	http.Handle("/", r) // 通过 router.go 中定义的路由器来分发请求
 
 	log.Println("Starting HTTP service at " + port)
-	err := http.ListenAndServe(":"+port, nil)
+	err := http.ListenAndServe(config.App.Address, nil)
 
 	if err != nil {
 		log.Println("An error occured starting http listener at port " + port)
